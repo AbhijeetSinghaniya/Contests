d6c2957de31c89feda41d6fea9a62cd20d6f882a
Abhijeet Singhaniya <2018abhijeet@gmail.com>
2022-05-13 02:38:14 +0530

    Update 3. Average Gift.cpp

--- CodeChef/CodeChef Starters 38/3. Average Gift.cpp
+++ CodeChef/CodeChef Starters 38/3. Average Gift.cpp
@@ -11,7 +11,7 @@ int main() {
         vector<int> a(n);
         for(int i=0;i<n;i++) cin>>a[i];
         
-        sort(a.begin(),a.end());
+        sort(a.begin(),a.end()); //can also find min and max and reduce T.C. to O(N) from O(NlogN)
         
         if(a[0] <= x && a[n-1] >= x) cout<<"YES"<<endl;
         else cout<<"NO"<<endl;
