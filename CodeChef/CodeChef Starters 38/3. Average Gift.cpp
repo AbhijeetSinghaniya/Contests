45ebd2c80bc54bde0112e4110cbfc964acd0c6f8
Abhijeet Singhaniya <2018abhijeet@gmail.com>
2022-05-13 02:34:48 +0530

    Create 3. Average Gift.cpp

---
+++ CodeChef/CodeChef Starters 38/3. Average Gift.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        
+        vector<int> a(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        
+        sort(a.begin(),a.end());
+        
+        if(a[0] <= x && a[n-1] >= x) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+	return 0;
+}
